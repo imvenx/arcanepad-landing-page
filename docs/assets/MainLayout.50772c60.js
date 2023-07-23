2eda6ec8d64693f54fa5dcc38b8e44db963e3f47
imvenx <imvenx@proton.me>
2023-07-23 16:43:57 +0200

    add public path

--- docs/assets/MainLayout.50772c60.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r,a as t,o as a,c as n}from"./index.958f1e18.js";const m=o({__name:"MainLayout",setup(s){return r(!1),(c,_)=>{const e=t("router-view");return a(),n(e)}}});export{m as default};
---
+++ docs/assets/MainLayout.5c7d5329.js
@@ -0,0 +1,1 @@
+import{d as o,r,a as t,o as a,c as n}from"./index.6bf08e89.js";const m=o({__name:"MainLayout",setup(s){return r(!1),(c,_)=>{const e=t("router-view");return a(),n(e)}}});export{m as default};
